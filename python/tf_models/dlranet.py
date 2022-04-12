5a5f4d788d6b42426f50d6117cdaf4db3cb54a67
Steffen <steffen.schotthoefer@kit.edu>
2022-04-12 15:39:02 -0400

    added tf version

--- python/tf_models/dlranet.py
+++ python/tf_models/dlranet.py
@@ -11,18 +11,38 @@ from tensorflow.keras import layers
 
 class PartDLRANet(keras.Model):
 
-    def __init__(self, name="referenceNet", **kwargs):
+    def __init__(self, name="partDLRANet", **kwargs):
         super(PartDLRANet, self).__init__(name=name, **kwargs)
+        self.denseBlock = DenseBlock()
+        self.dlraBlock = DLRALayer(units=250, low_rank=100)
+        self.outputBlock = DenseBlockOutput()
+
+    def call(self, inputs, step: int):
+        z = self.denseBlock(inputs)
+        z = self.dlraBlock(z, step=step)
+        z = self.outputBlock(z)
+        return z
+
+
+class DenseBlock(keras.layers.Layer):
+    def __init__(self, name="dense_block", **kwargs):
+        super(DenseBlock, self).__init__(name=name, **kwargs)
         self.layer1 = Linear(units=64)
-        self.layer2 = Linear(units=250)
-        self.layer3 = Linear(units=250)
-        self.layer4 = Linear(units=1)
 
     def call(self, inputs):
         z = self.layer1(inputs)
         z = tf.keras.activations.relu(z)
-        z = self.layer2(z)
-        z = tf.keras.activations.relu(z)
+        return z
+
+
+class DenseBlockOutput(keras.layers.Layer):
+    def __init__(self, name="dense_output_block", **kwargs):
+        super(DenseBlockOutput, self).__init__(name=name, **kwargs)
+        self.layer3 = Linear(units=250)
+        self.layer4 = Linear(units=1)
+
+    def call(self, inputs):
+        z = tf.keras.activations.relu(inputs)
         z = self.layer3(z)
         z = tf.keras.activations.relu(z)
         z = self.layer4(z)
@@ -30,22 +50,83 @@ class PartDLRANet(keras.Model):
 
 
 class DLRALayer(keras.layers.Layer):
-    def __init__(self, units=32, rank=10, **kwargs):
+    def __init__(self, units=32, low_rank=10, name="dlra_block", **kwargs):
         super(DLRALayer, self).__init__(**kwargs)
         self.units = units
-        self.rank = rank
+        self.low_rank = low_rank
 
     def build(self, input_shape):
-        self.K = self.add_weight(shape=(input_shape[-1], self.units), initializer="random_normal",
+        self.k = self.add_weight(shape=(input_shape[-1], self.low_rank), initializer="random_normal",
+                                 trainable=True, )
+        self.l_t = self.add_weight(shape=(self.units, self.low_rank), initializer="random_normal",
+                                   trainable=True, )
+        self.s = self.add_weight(shape=(self.low_rank, self.low_rank), initializer="random_normal",
                                  trainable=True, )
         self.b = self.add_weight(shape=(self.units,), initializer="random_normal", trainable=True)
+        # auxiliary variables
+        self.aux_U = self.add_weight(shape=(self.units, self.low_rank), initializer="random_normal",
+                                     trainable=False, )
+        self.aux_Unp1 = self.add_weight(shape=(self.units, self.low_rank), initializer="random_normal",
+                                        trainable=False, )
+        self.aux_Vt = self.add_weight(shape=(self.low_rank, self.units), initializer="random_normal",
+                                      trainable=False, )
+        self.aux_Vtnp1 = self.add_weight(shape=(self.low_rank, self.units), initializer="random_normal",
+                                         trainable=False, )
+        self.aux_N = self.add_weight(shape=(self.low_rank, self.low_rank), initializer="random_normal",
+                                     trainable=False, )
+        self.aux_M = self.add_weight(shape=(self.low_rank, self.low_rank), initializer="random_normal",
+                                     trainable=False, )
 
-    def call(self, inputs):
-        return tf.matmul(inputs, self.w) + self.b
+    def call(self, inputs, step):
+        """
+        :param inputs: layer input
+        :param step: step conter: k:= 0, l:=1, s:=2
+        :return:
+        """
+        if step == 0:  # k-step
+            z = tf.matmul(inputs, tf.matmul(self.k, self.aux_Vt))
+        elif step == 1:  # l-step
+            z = tf.matmul(inputs, tf.matmul(self.aux_U, self.l_t))
+        else:  # s-step
+            z = tf.matmul(inputs, tf.matmul(self.aux_Unp1, tf.matmul(self.s, self.aux_Vtnp1)))
+        return z
+
+    def k_step_preprocessing(self, ):
+        k = tf.matmul(self.aux_U, self.s)
+        self.k = tf.Variable(initial_value=k, trainable=True, )
+        return 0
+
+    def k_step_postprocessing(self):
+        aux_Unp1, _ = tf.linalg.qr(self.k)
+        self.aux_Unp1 = tf.Variable(initial_value=aux_Unp1, trainable=False, )
+        self.aux_N = tf.matmul(tf.transpose(self.aux_Unp1), self.aux_U)
+        return 0
+
+    def l_step_preprocessing(self, ):
+        l_t = tf.matmul(self.s, self.aux_Vt)
+        self.l_t = tf.Variable(initial_value=l_t, trainable=True, )
+        return 0
+
+    def l_step_postpocessing(self):
+        aux_Vtnp1, _ = tf.linalg.qr(tf.transpose(self.l_t))
+        self.aux_Vtnp1 = tf.transpose(aux_Vtnp1)
+        self.aux_M = tf.matmul(self.aux_Vtnp1, tf.transpose(self.aux_Vt))
+        return 0
+
+    def s_step_preprocessing(self):
+        s = tf.matmul(tf.matmul(self.aux_N, self.s), tf.transpose(self.aux_M))
+        self.s = tf.Variable(initial_value=s, trainable=True, )
+        return 0
+
+    def s_step_postprocessing(self):
+        self.aux_U = self.aux_Unp1
+        self.aux_Vt = self.aux_Vtnp1
+        return 0
 
     def get_config(self):
-        config = super(Linear, self).get_config()
+        config = super(DLRALayer, self).get_config()
         config.update({"units": self.units})
+        config.update({"low_rank": self.low_rank})
         return config
 
 
--- python/tf_models/trainingscript.py
+++ python/tf_models/trainingscript.py
@@ -1,8 +1,59 @@
-from dlranet import VariationalAutoEncoder, ReferenceNet
+from dlranet import VariationalAutoEncoder, ReferenceNet, PartDLRANet
 
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+def main3():
+    model = PartDLRANet()
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
+    mse_loss_fn = tf.keras.losses.MeanSquaredError()
+
+    loss_metric = tf.keras.metrics.Mean()
+
+    # Build dataset
+    n_train = 10000
+    n_test = 10000
+
+    train_x = np.linspace(-2, 2, n_train).reshape((n_train, 1))
+    train_y = np.square(train_x)
+
+    test_x = np.linspace(-2, 2, n_test).reshape((n_test, 1))
+    test_y = np.square(test_x)
+
+    train_dataset = tf.data.Dataset.from_tensor_slices((train_x, train_y))
+    train_dataset = train_dataset.shuffle(buffer_size=1024).batch(32)
+
+    z = model.dlraBlock.variables
+    epochs = 10
+
+    # Iterate over epochs.
+    for epoch in range(epochs):
+        print("Start of epoch %d" % (epoch,))
+
+        # Iterate over the batches of the dataset.
+        for step, batch_train in enumerate(train_dataset):
+            with tf.GradientTape() as tape:
+                out = model(batch_train[0], step=0)
+                # Compute reconstruction loss
+                loss = mse_loss_fn(batch_train[1], out)
+                loss += sum(model.losses)  # Add KLD regularization loss
+
+            grads = tape.gradient(loss, model.trainable_weights)
+            optimizer.apply_gradients(zip(grads, model.trainable_weights))
+
+            loss_metric(loss)
+
+            if step % 100 == 0:
+                print("step %d: mean loss = %.4f" % (step, loss_metric.result()))
+
+    test = model(test_x, step=0)
+    plt.plot(test_x, test.numpy(), '-.')
+    plt.plot(test_x, test_y, '--')
+    plt.show()
+    return 0
 
 
 def main2():
@@ -98,4 +149,4 @@ def main():
 
 if __name__ == '__main__':
     # main()
-    main2()
+    main3()
