485dd9e5d72b0659d064e6bc8796df2fcf232e96
ScSteffen <steffen.schotthoefer@outlook.de>
2022-04-21 22:25:07 +0200

    change dlra params

--- python/tf_models/dlranet.py
+++ python/tf_models/dlranet.py
@@ -11,10 +11,10 @@ from tensorflow.keras import layers
 
 class PartDLRANet(keras.Model):
 
-    def __init__(self, input_dim=1, output_dim=1, name="partDLRANet", tol=0.4, low_rank=20,rmax_total=100, **kwargs):
+    def __init__(self, input_dim=1, output_dim=1, name="partDLRANet", tol=0.4, low_rank=1000,rmax_total=3000, **kwargs):
         super(PartDLRANet, self).__init__(name=name, **kwargs)
-        self.denseBlock = DenseBlock(units=250, input_dim=input_dim)
-        self.dlraBlock = DLRALayer(input_dim=250, units=250, low_rank=low_rank,epsAdapt=tol, rmax_total=rmax_total,)
+        self.denseBlock = DenseBlock(units=3000, input_dim=input_dim)
+        self.dlraBlock = DLRALayer(input_dim=3000, units=3000, low_rank=low_rank,epsAdapt=tol, rmax_total=rmax_total)
         self.outputBlock = DenseBlockOutput(output_dim=output_dim)
 
     def call(self, inputs, step: int):
