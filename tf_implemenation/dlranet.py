da378f745eb10d7adc2d9b41db2ce5102812fed0
Steffen <steffen.schotthoefer@kit.edu>
2022-05-03 12:24:18 -0400

    add dlranet with dense output block

--- tf_implemenation/dlranet.py
+++ tf_implemenation/dlranet.py
@@ -10,6 +10,82 @@ from os import path, makedirs
 import numpy as np
 
 
+class DLRANetDenseOut(keras.Model):
+
+    def __init__(self, input_dim=1, output_dim=1, name="e2eDLRANet", tol=0.4, low_rank=20, dlra_layer_dim=200,
+                 rmax_total=100, **kwargs):
+        super(DLRANetDenseOut, self).__init__(name=name, **kwargs)
+        # dlra_layer_dim = 250
+        self.dlraBlockInput = DLRALayer(input_dim=input_dim, units=dlra_layer_dim, low_rank=dlra_layer_dim,
+                                        epsAdapt=tol,
+                                        rmax_total=rmax_total, )
+        self.dlraBlock1 = DLRALayer(input_dim=dlra_layer_dim, units=dlra_layer_dim, low_rank=low_rank, epsAdapt=tol,
+                                    rmax_total=rmax_total, )
+        self.dlraBlock2 = DLRALayer(input_dim=dlra_layer_dim, units=dlra_layer_dim, low_rank=low_rank, epsAdapt=tol,
+                                    rmax_total=rmax_total, )
+        self.dlraBlock3 = DLRALayer(input_dim=dlra_layer_dim, units=dlra_layer_dim, low_rank=low_rank, epsAdapt=tol,
+                                    rmax_total=rmax_total, )
+        self.dlraBlockOutput = Linear(input_dim=dlra_layer_dim, units=output_dim)
+
+    def call(self, inputs, step: int = 0):
+        z = self.dlraBlockInput(inputs, step=step)
+        z = self.dlraBlock1(z, step=step)
+        z = self.dlraBlock2(z, step=step)
+        z = self.dlraBlock3(z, step=step)
+        z = self.dlraBlockOutput(z)
+        return z
+
+    @staticmethod
+    def set_none_grads_to_zero(grads, weights):
+        """
+        :param grads: gradients of current tape
+        :param weights: weights of current model
+        :return: sets the nonexistent gradients to zero (i K step, the grads of S and L step are None, which throws annoying warnings)
+        """
+        for i in range(len(grads)):
+            if grads[i] is None:
+                grads[i] = tf.zeros(shape=weights[i].shape, dtype=tf.float32)
+        return 0
+
+    @staticmethod
+    def set_dlra_bias_grads_to_zero(grads):
+        """
+        :param grads: gradients of current tape
+        :return: sets the nonexistent gradients to zero (i K step, the grads of S and L step are None, which throws annoying warnings)
+        """
+        for i in range(len(grads)):
+            if len(grads[i].shape) == 1:
+                grads[i] = tf.math.scalar_mul(0.0, grads[i])
+        return 0
+
+    def toggle_non_s_step_training(self):
+        # self.layers[0].trainable = False  # Dense input
+        self.layers[-1].trainable = False  # Dense output
+
+        return 0
+
+    def toggle_s_step_training(self):
+        # self.layers[0].trainable = True  # Dense input
+        self.layers[-1].trainable = True  # Dense output
+        return 0
+
+    def save(self, folder_name):
+        self.dlraBlockInput.save(folder_name=folder_name, layer_id=0)
+        self.dlraBlock1.save(folder_name=folder_name, layer_id=1)
+        self.dlraBlock2.save(folder_name=folder_name, layer_id=2)
+        self.dlraBlock3.save(folder_name=folder_name, layer_id=3)
+        self.dlraBlockOutput.save(folder_name=folder_name)
+        return 0
+
+    def load(self, folder_name):
+        self.dlraBlockInput.load(folder_name=folder_name, layer_id=0)
+        self.dlraBlock1.load(folder_name=folder_name, layer_id=1)
+        self.dlraBlock2.load(folder_name=folder_name, layer_id=2)
+        self.dlraBlock3.load(folder_name=folder_name, layer_id=3)
+        self.dlraBlockOutput.load(folder_name=folder_name, layer_id=4)
+        return 0
+
+
 class END2ENDDLRANet(keras.Model):
 
     def __init__(self, input_dim=1, output_dim=1, name="e2eDLRANet", tol=0.4, low_rank=20, dlra_layer_dim=200,
