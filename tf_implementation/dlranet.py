44c7b19106678afd2fa9899032083b09925c828b
Steffen <steffen.schotthoefer@kit.edu>
2022-05-09 15:03:50 -0400

    s training added

--- tf_implementation/dlranet.py
+++ tf_implementation/dlranet.py
@@ -10,6 +10,103 @@ from os import path, makedirs
 import numpy as np
 
 
+class DLRANetSTrain(keras.Model):
+
+    def __init__(self, input_dim=1, output_dim=1, name="e2eDLRANet", tol=0.4, low_rank=20, dlra_layer_dim=200,
+                 rmax_total=100, **kwargs):
+        super(DLRANetSTrain, self).__init__(name=name, **kwargs)
+        # dlra_layer_dim = 250
+        self.input_dim = input_dim
+        self.dlra_layer_dim = dlra_layer_dim
+        self.low_rank = low_rank
+        self.output_dim = output_dim
+        self.tol = tol
+        self.rmax_total = rmax_total
+
+        self.dlraBlockInput = DLRALayerSTrain(input_dim=self.input_dim, units=self.dlra_layer_dim,
+                                              low_rank=self.low_rank,
+                                              epsAdapt=self.tol, rmax_total=self.rmax_total, )
+        self.dlraBlock1 = DLRALayerSTrain(input_dim=self.dlra_layer_dim, units=self.dlra_layer_dim,
+                                          low_rank=self.low_rank,
+                                          epsAdapt=self.tol,
+                                          rmax_total=self.rmax_total, )
+        self.dlraBlock2 = DLRALayerSTrain(input_dim=self.dlra_layer_dim, units=self.dlra_layer_dim,
+                                          low_rank=self.low_rank,
+                                          epsAdapt=self.tol,
+                                          rmax_total=self.rmax_total, )
+        self.dlraBlock3 = DLRALayerSTrain(input_dim=self.dlra_layer_dim, units=self.dlra_layer_dim,
+                                          low_rank=self.low_rank,
+                                          epsAdapt=self.tol,
+                                          rmax_total=rmax_total, )
+        self.dlraBlockOutput = Linear(input_dim=self.dlra_layer_dim, units=self.output_dim)
+
+    def build_model(self):
+        self.dlraBlockInput.build_model()
+        self.dlraBlock1.build_model()
+        self.dlraBlock2.build_model()
+        self.dlraBlock3.build_model()
+        return 0
+
+    @tf.function
+    def call(self, inputs, step: int = 0):
+        z = self.dlraBlockInput(inputs, step=step)
+        z = self.dlraBlock1(z, step=step)
+        z = self.dlraBlock2(z, step=step)
+        z = self.dlraBlock3(z, step=step)
+        z = self.dlraBlockOutput(z)
+        return z
+
+    @staticmethod
+    def set_none_grads_to_zero(grads, weights):
+        """
+        :param grads: gradients of current tape
+        :param weights: weights of current model
+        :return: sets the nonexistent gradients to zero (i K step, the grads of S and L step are None, which throws annoying warnings)
+        """
+        for i in range(len(grads)):
+            if grads[i] is None:
+                grads[i] = tf.zeros(shape=weights[i].shape, dtype=tf.float32)
+        return 0
+
+    @staticmethod
+    def set_dlra_bias_grads_to_zero(grads):
+        """
+        :param grads: gradients of current tape
+        :return: sets the nonexistent gradients to zero (i K step, the grads of S and L step are None, which throws annoying warnings)
+        """
+        for i in range(len(grads)):
+            if len(grads[i].shape) == 1:
+                grads[i] = tf.math.scalar_mul(0.0, grads[i])
+        return 0
+
+    def toggle_non_s_step_training(self):
+        # self.layers[0].trainable = False  # Dense input
+        self.layers[-1].trainable = False  # Dense output
+
+        return 0
+
+    def toggle_s_step_training(self):
+        # self.layers[0].trainable = True  # Dense input
+        self.layers[-1].trainable = True  # Dense output
+        return 0
+
+    def save(self, folder_name):
+        self.dlraBlockInput.save(folder_name=folder_name, layer_id=0)
+        self.dlraBlock1.save(folder_name=folder_name, layer_id=1)
+        self.dlraBlock2.save(folder_name=folder_name, layer_id=2)
+        self.dlraBlock3.save(folder_name=folder_name, layer_id=3)
+        self.dlraBlockOutput.save(folder_name=folder_name)
+        return 0
+
+    def load(self, folder_name):
+        self.dlraBlockInput.load(folder_name=folder_name, layer_id=0)
+        self.dlraBlock1.load(folder_name=folder_name, layer_id=1)
+        self.dlraBlock2.load(folder_name=folder_name, layer_id=2)
+        self.dlraBlock3.load(folder_name=folder_name, layer_id=3)
+        self.dlraBlockOutput.load(folder_name=folder_name)
+        return 0
+
+
 class DLRANet(keras.Model):
 
     def __init__(self, input_dim=1, output_dim=1, name="e2eDLRANet", tol=0.4, low_rank=20, dlra_layer_dim=200,
@@ -19,27 +116,30 @@ class DLRANet(keras.Model):
         self.input_dim = input_dim
         self.dlra_layer_dim = dlra_layer_dim
         self.low_rank = low_rank
-        self.output_dim=output_dim
-        self.tol =tol
-        self.rmax_total=rmax_total
-        
+        self.output_dim = output_dim
+        self.tol = tol
+        self.rmax_total = rmax_total
+
         self.dlraBlockInput = DLRALayer(input_dim=self.input_dim, units=self.dlra_layer_dim, low_rank=self.low_rank,
-                                        epsAdapt=self.tol,                                        rmax_total=self.rmax_total, )
-        self.dlraBlock1 = DLRALayer(input_dim=self.dlra_layer_dim, units=self.dlra_layer_dim, low_rank=self.low_rank, epsAdapt=self.tol,
+                                        epsAdapt=self.tol, rmax_total=self.rmax_total, )
+        self.dlraBlock1 = DLRALayer(input_dim=self.dlra_layer_dim, units=self.dlra_layer_dim, low_rank=self.low_rank,
+                                    epsAdapt=self.tol,
                                     rmax_total=self.rmax_total, )
-        self.dlraBlock2 = DLRALayer(input_dim=self.dlra_layer_dim, units=self.dlra_layer_dim, low_rank=self.low_rank, epsAdapt=self.tol,
+        self.dlraBlock2 = DLRALayer(input_dim=self.dlra_layer_dim, units=self.dlra_layer_dim, low_rank=self.low_rank,
+                                    epsAdapt=self.tol,
                                     rmax_total=self.rmax_total, )
-        self.dlraBlock3 = DLRALayer(input_dim=self.dlra_layer_dim, units=self.dlra_layer_dim, low_rank=self.low_rank, epsAdapt=self.tol,
+        self.dlraBlock3 = DLRALayer(input_dim=self.dlra_layer_dim, units=self.dlra_layer_dim, low_rank=self.low_rank,
+                                    epsAdapt=self.tol,
                                     rmax_total=rmax_total, )
         self.dlraBlockOutput = Linear(input_dim=self.dlra_layer_dim, units=self.output_dim)
-    
+
     def build_model(self):
         self.dlraBlockInput.build_model()
         self.dlraBlock1.build_model()
         self.dlraBlock2.build_model()
         self.dlraBlock3.build_model()
         return 0
-    
+
     @tf.function
     def call(self, inputs, step: int = 0):
         z = self.dlraBlockInput(inputs, step=step)
@@ -220,7 +320,7 @@ class DLRALayer(keras.layers.Layer):
         self.low_rank = low_rank
         self.rmax_total = rmax_total
         self.input_dim = input_dim
-        
+
     def build_model(self):
 
         self.k = self.add_weight(shape=(self.input_dim, self.low_rank), initializer="random_normal",
@@ -245,7 +345,7 @@ class DLRALayer(keras.layers.Layer):
                                      trainable=False, name="aux_M")
         # Todo: initializer with low rank
         return 0
-    
+
     @tf.function
     def call(self, inputs, step: int = 0):
         """
@@ -393,6 +493,212 @@ class DLRALayer(keras.layers.Layer):
                                trainable=True, name="lt_", dtype=tf.float32)
         s_np = np.load(folder_name + "/s" + str(layer_id) + ".npy")
         self.s = tf.Variable(initial_value=s_np,
+                             trainable=True, name="s_", dtype=tf.float32)
+        # aux variables
+        aux_U_np = np.load(folder_name + "/aux_U" + str(layer_id) + ".npy")
+        self.aux_U = tf.Variable(initial_value=aux_U_np,
+                                 trainable=True, name="aux_U", dtype=tf.float32)
+        aux_Unp1_np = np.load(folder_name + "/aux_Unp1" + str(layer_id) + ".npy")
+        self.aux_Unp1 = tf.Variable(initial_value=aux_Unp1_np,
+                                    trainable=True, name="aux_Unp1", dtype=tf.float32)
+        Vt_np = np.load(folder_name + "/aux_Vt" + str(layer_id) + ".npy")
+        self.aux_Vt = tf.Variable(initial_value=Vt_np,
+                                  trainable=True, name="Vt", dtype=tf.float32)
+        vtnp1_np = np.load(folder_name + "/aux_Vtnp1" + str(layer_id) + ".npy")
+        self.aux_Vtnp1 = tf.Variable(initial_value=vtnp1_np,
+                                     trainable=True, name="vtnp1", dtype=tf.float32)
+        aux_N_np = np.load(folder_name + "/aux_N" + str(layer_id) + ".npy")
+        self.aux_N = tf.Variable(initial_value=aux_N_np,
+                                 trainable=True, name="aux_N", dtype=tf.float32)
+        aux_M_np = np.load(folder_name + "/aux_M" + str(layer_id) + ".npy")
+        self.aux_M = tf.Variable(initial_value=aux_M_np,
+                                 trainable=True, name="aux_M", dtype=tf.float32)
+        return 0
+
+
+class DLRALayerSTrain(keras.layers.Layer):
+    def __init__(self, input_dim: int, units=32, low_rank=10, epsAdapt=0.1, rmax_total=100, name="dlra_block",
+                 **kwargs):
+        super(DLRALayerSTrain, self).__init__(**kwargs)
+        self.epsAdapt = epsAdapt  # for unconventional integrator
+        self.units = units
+        self.low_rank = low_rank
+        self.rmax_total = rmax_total
+        self.input_dim = input_dim
+
+    def build_model(self):
+
+        self.k = self.add_weight(shape=(self.input_dim, self.low_rank), initializer="random_normal",
+                                 trainable=True, name="k_")
+        self.l_t = self.add_weight(shape=(self.low_rank, self.units), initializer="random_normal",
+                                   trainable=True, name="lt_")
+        self.s = self.add_weight(shape=(self.low_rank, self.low_rank), initializer="random_normal",
+                                 trainable=True, name="s_")
+        self.b = self.add_weight(shape=(self.units,), initializer="random_normal", trainable=True, name="b_")
+        # auxiliary variables
+        self.aux_U = self.add_weight(shape=(self.input_dim, self.low_rank), initializer="random_normal",
+                                     trainable=False, name="aux_U")
+        self.aux_Unp1 = self.add_weight(shape=(self.input_dim, self.low_rank), initializer="random_normal",
+                                        trainable=False, name="aux_Unp1")
+        self.aux_Vt = self.add_weight(shape=(self.low_rank, self.units), initializer="random_normal",
+                                      trainable=False, name="Vt")
+        self.aux_Vtnp1 = self.add_weight(shape=(self.low_rank, self.units), initializer="random_normal",
+                                         trainable=False, name="vtnp1")
+        self.aux_N = self.add_weight(shape=(self.low_rank, self.low_rank), initializer="random_normal",
+                                     trainable=False, name="aux_N")
+        self.aux_M = self.add_weight(shape=(self.low_rank, self.low_rank), initializer="random_normal",
+                                     trainable=False, name="aux_M")
+        # Todo: initializer with low rank
+        return 0
+
+    @tf.function
+    def call(self, inputs, step: int = 0):
+        """
+        :param inputs: layer input
+        :param step: step conter: k:= 0, l:=1, s:=2
+        :return:
+        """
+        if step == 0:  # k-step
+            z = tf.matmul(tf.matmul(inputs, self.k), self.aux_Vt)
+        elif step == 1:  # l-step
+            z = tf.matmul(tf.matmul(inputs, self.aux_U), self.l_t)
+        else:  # s-step
+            z = tf.matmul(tf.matmul(tf.matmul(inputs, self.aux_Unp1), self.s), self.aux_Vtnp1)
+        return tf.keras.activations.relu(z + self.b)
+
+    @tf.function
+    def call(self, inputs, step: int = 0):
+        """
+        :param inputs: layer input
+        :param step: step conter: k:= 0, l:=1, s:=2
+        :return:
+        """
+        if step == 0:  # k-step
+            z = tf.matmul(tf.matmul(inputs, self.k), self.aux_Vt)
+        elif step == 1:  # l-step
+            z = tf.matmul(tf.matmul(inputs, self.aux_U), self.l_t)
+        else:  # s-step
+            z = tf.matmul(tf.matmul(tf.matmul(inputs, self.aux_Unp1), self.s), self.aux_Vtnp1)
+        return tf.keras.activations.relu(z + self.b)
+
+    @tf.function
+    def k_step_preprocessing(self, ):
+        k = tf.matmul(self.aux_U, self.s)
+        self.k.assign(k)  # = tf.Variable(initial_value=k, trainable=True, name="k_")
+        return 0
+
+    @tf.function
+    def k_step_postprocessing(self):
+        aux_Unp1, _ = tf.linalg.qr(self.k)
+        self.aux_Unp1.assign(aux_Unp1)  # = tf.Variable(initial_value=aux_Unp1, trainable=False, name="aux_Unp1")
+        N = tf.matmul(tf.transpose(self.aux_Unp1), self.aux_U)
+        self.aux_N.assign(N)
+        return 0
+
+    def k_step_postprocessing_adapt(self):
+        aux_Unp1, _ = tf.linalg.qr(tf.concat((self.k, self.aux_U), axis=1))
+        self.aux_Unp1 = tf.Variable(initial_value=aux_Unp1, trainable=False, name="aux_Unp1")
+        self.aux_N = tf.matmul(tf.transpose(self.aux_Unp1), self.aux_U)
+        return 0
+
+    @tf.function
+    def l_step_preprocessing(self, ):
+        l_t = tf.matmul(self.s, self.aux_Vt)
+        self.l_t.assign(l_t)  # = tf.Variable(initial_value=l_t, trainable=True, name="lt_")
+        return 0
+
+    @tf.function
+    def l_step_postprocessing(self):
+        aux_Vtnp1, _ = tf.linalg.qr(tf.transpose(self.l_t))
+        self.aux_Vtnp1.assign(tf.transpose(aux_Vtnp1))
+        M = tf.matmul(self.aux_Vtnp1, tf.transpose(self.aux_Vt))
+        self.aux_M.assign(M)
+        return 0
+
+    def l_step_postprocessing_adapt(self):
+        aux_Vtnp1, _ = tf.linalg.qr(tf.concat((tf.transpose(self.l_t), tf.transpose(self.aux_Vt)), axis=1))
+        self.aux_Vtnp1 = tf.transpose(aux_Vtnp1)
+        self.aux_M = tf.matmul(self.aux_Vtnp1, tf.transpose(self.aux_Vt))
+        return 0
+
+    @tf.function
+    def s_step_preprocessing(self):
+        self.aux_U.assign(self.aux_Unp1)
+        self.aux_Vt.assign(self.aux_Vtnp1)
+        s = tf.matmul(tf.matmul(self.aux_N, self.s), tf.transpose(self.aux_M))
+        self.s.assign(s)  # = tf.Variable(initial_value=s, trainable=True, name="s_")
+        return 0
+
+    def rank_adaption(self):
+        # 1) compute SVD of S
+        d, u2, v2 = tf.linalg.svd(self.s)  # d=singular values, u2 = left singuar vecs, v2= right singular vecss
+        # print(d.shape)
+        tmp = 0.0
+        tol = self.epsAdapt * tf.linalg.norm(d)  # absolute value treshold (try also relative one)
+        rmax = int(tf.floor(d.shape[0] / 2))
+        for j in range(0, 2 * rmax - 1):
+            tmp = tf.linalg.norm(d[j:2 * rmax - 1])
+            if tmp < tol:
+                rmax = j
+                break
+
+        rmax = tf.minimum(rmax, self.rmax_total)
+        rmax = tf.maximum(rmax, 2)
+
+        # update s
+        self.s = tf.linalg.tensor_diag(d[:rmax])
+        # self.s = s
+
+        # update u and v
+        self.aux_U = tf.matmul(self.aux_U, u2[:, :rmax])
+        self.aux_Vt = tf.matmul(v2[:rmax, :], self.aux_Vt)
+        self.low_rank = rmax
+        return 0
+
+    def get_config(self):
+        config = super(DLRALayer, self).get_config()
+        config.update({"units": self.units})
+        config.update({"low_rank": self.low_rank})
+        return config
+
+    def save(self, folder_name, layer_id):
+        # main_variables
+        k_np = self.k.numpy()
+        np.save(folder_name + "/k" + str(layer_id) + ".npy", k_np)
+        l_t_np = self.l_t.numpy()
+        np.save(folder_name + "/l_t" + str(layer_id) + ".npy", l_t_np)
+        s_np = self.s.numpy()
+        np.save(folder_name + "/s" + str(layer_id) + ".npy", s_np)
+        b_np = self.b.numpy()
+        np.save(folder_name + "/b" + str(layer_id) + ".npy", b_np)
+        # aux_variables
+        aux_U_np = self.aux_U.numpy()
+        np.save(folder_name + "/aux_U" + str(layer_id) + ".npy", aux_U_np)
+        aux_Unp1_np = self.aux_Unp1.numpy()
+        np.save(folder_name + "/aux_Unp1" + str(layer_id) + ".npy", aux_Unp1_np)
+        aux_Vt_np = self.aux_Vt.numpy()
+        np.save(folder_name + "/aux_Vt" + str(layer_id) + ".npy", aux_Vt_np)
+        aux_Vtnp1_np = self.aux_Vtnp1.numpy()
+        np.save(folder_name + "/aux_Vtnp1" + str(layer_id) + ".npy", aux_Vtnp1_np)
+        aux_N_np = self.aux_N.numpy()
+        np.save(folder_name + "/aux_N" + str(layer_id) + ".npy", aux_N_np)
+        aux_M_np = self.aux_M.numpy()
+        np.save(folder_name + "/aux_M" + str(layer_id) + ".npy", aux_M_np)
+        return 0
+
+    def load(self, folder_name, layer_id):
+
+        # main variables
+        k_np = np.load(folder_name + "/k" + str(layer_id) + ".npy")
+        self.low_rank = k_np.shape[1]
+        self.k = tf.Variable(initial_value=k_np,
+                             trainable=True, name="k_", dtype=tf.float32)
+        l_t_np = np.load(folder_name + "/l_t" + str(layer_id) + ".npy")
+        self.l_t = tf.Variable(initial_value=l_t_np,
+                               trainable=True, name="lt_", dtype=tf.float32)
+        s_np = np.load(folder_name + "/s" + str(layer_id) + ".npy")
+        s_random = np.random.normal(size=s_np.shape)
+        self.s = tf.Variable(initial_value=s_random,
                              trainable=True, name="s_", dtype=tf.float32)
         # aux variables
         aux_U_np = np.load(folder_name + "/aux_U" + str(layer_id) + ".npy")
---
+++ tf_implementation/run_tests_fixed_rank_train_s.sh
@@ -0,0 +1,9 @@
+
+python mnist_DLRA_fixed_rank_s_train.py -s 350 -t 0.17 -l 1 -a 1
+#python mnist_DLRA_fixed_rank_s_train.py -s 350 -t 0.15 -l 1 -a 1
+#python mnist_DLRA_fixed_rank_s_train.py -s 350 -t 0.13 -l 1 -a 1
+#python mnist_DLRA_fixed_rank_s_train.py -s 350 -t 0.11 -l 1 -a 1
+#python mnist_DLRA_fixed_rank_s_train.py -s 350 -t 0.09 -l 1 -a 1
+#python mnist_DLRA_fixed_rank_s_train.py -s 350 -t 0.07 -l 1 -a 1
+#python mnist_DLRA_fixed_rank_s_train.py -s 350 -t 0.05 -l 1 -a 1
+#python mnist_DLRA_fixed_rank_s_train.py -s 350 -t 0.03 -l 1 -a 1
