64c3c47e45a0fa1645422010a516b3f8659bf418
Jonas Kusch <jonas.kusch@kit.edu>
2022-08-23 11:43:21 +0200

    started transformers

--- src/speech_transformer.py
+++ src/speech_transformer.py
@@ -1,10 +1,62 @@
-from networks.transformer import BaseTransformer, create_csv_logger_cb
+from networks.transformer import Translator
 
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 from optparse import OptionParser
 from os import path, makedirs
+import matplotlib.pyplot as plt
+
+import tensorflow_datasets as tfds
+
+# global constants
+MAX_TOKENS = 128
+BUFFER_SIZE = 20000
+BATCH_SIZE = 64
+
+def filter_max_tokens(pt, en):
+  num_tokens = tf.maximum(tf.shape(pt)[1],tf.shape(en)[1])
+  return num_tokens < MAX_TOKENS
+
+def tokenize_pairs(pt, en):
+    pt = tokenizers.pt.tokenize(pt)
+    # Convert from ragged to dense, padding with zeros.
+    pt = pt.to_tensor()
+
+    en = tokenizers.en.tokenize(en)
+    # Convert from ragged to dense, padding with zeros.
+    en = en.to_tensor()
+    return pt, en
+
+def make_batches(ds):
+  return (
+      ds
+      .cache()
+      .shuffle(BUFFER_SIZE)
+      .batch(BATCH_SIZE)
+      .map(tokenize_pairs, num_parallel_calls=tf.data.AUTOTUNE)
+      .filter(filter_max_tokens)
+      .prefetch(tf.data.AUTOTUNE))
+    
+def loss_function(real, pred):
+    mask = tf.math.logical_not(tf.math.equal(real, 0))
+    loss_ = loss_object(real, pred)
+
+    mask = tf.cast(mask, dtype=loss_.dtype)
+    loss_ *= mask
+
+    return tf.reduce_sum(loss_)/tf.reduce_sum(mask)
+
+
+def accuracy_function(real, pred):
+    accuracies = tf.equal(real, tf.argmax(pred, axis=2))
+
+    mask = tf.math.logical_not(tf.math.equal(real, 0))
+    accuracies = tf.math.logical_and(mask, accuracies)
+
+    accuracies = tf.cast(accuracies, dtype=tf.float32)
+    mask = tf.cast(mask, dtype=tf.float32)
+    return tf.reduce_sum(accuracies)/tf.reduce_sum(mask)
 
 
 def train(start_rank, tolerance, load_model, dim_layer, rmax, epochs):
@@ -24,166 +76,150 @@ def train(start_rank, tolerance, load_model, dim_layer, rmax, epochs):
 
     print("save model as: " + filename)
 
-    # Create Model
-    input_dim = 784  # 28x28  pixel per image
-    output_dim = 10  # one-hot vector of digits 0-9
+    # load dataset
+    examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en', with_info=True,
+                               as_supervised=True)
+    train_examples, val_examples = examples['train'], examples['validation']
 
-    starting_rank = start_rank  # starting rank of S matrix
-    tol = tolerance  # eigenvalue treshold
+    for pt_examples, en_examples in train_examples.batch(3).take(1):
+        for pt in pt_examples.numpy():
+            print(pt.decode('utf-8'))
 
-    max_rank = rmax  # maximum rank of S matrix
+    print()
 
-    dlra_layer_dim = dim_layer
+    for en in en_examples.numpy():
+        print(en.decode('utf-8'))
 
-    model = BaseTransformer(input_dim=input_dim, output_dim=output_dim, low_rank=starting_rank,
-                            dlra_layer_dim=dlra_layer_dim, tol=tol, rmax_total=max_rank)
-    # Build optimizer
-    optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
-    # Choose loss
-    loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=False)
-    # Choose metrics (to monitor training, but not to optimize on)
-    loss_metric = tf.keras.metrics.Mean()
-    acc_metric = tf.keras.metrics.Accuracy()
-    loss_metric_acc_val = tf.keras.metrics.Accuracy()
+    # Text tokenization & detokenization
+    model_name = 'ted_hrlr_translate_pt_en_converter'
+    tf.keras.utils.get_file(
+    f'{model_name}.zip',
+    f'https://storage.googleapis.com/download.tensorflow.org/models/{model_name}.zip',
+    cache_dir='.', cache_subdir='', extract=True
+    )
 
-    # Build dataset
-    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
-    x_train = np.reshape(x_train, (-1, input_dim))
-    x_test = np.reshape(x_test, (-1, input_dim))
+    tokenizers = tf.saved_model.load(model_name)
+    encoded = tokenizers.en.tokenize(en_examples)
+    tokens = tokenizers.en.lookup(encoded)
 
-    # Reserve 10,000 samples for validation.
-    val_size = 10000
-    x_val = x_train[-val_size:]
-    y_val = y_train[-val_size:]
-    (x_val, y_val) = normalize_img(x_val, y_val)
+    # investigate data
+    lengths = []
 
-    x_train = x_train[:-val_size]
-    y_train = y_train[:-val_size]
-    (x_train, y_train) = normalize_img(x_train, y_train)
+    for pt_examples, en_examples in train_examples.batch(1024):
+        pt_tokens = tokenizers.en.tokenize(pt_examples)
+        lengths.append(pt_tokens.row_lengths())
 
-    (x_test, y_test) = normalize_img(x_test, y_test)
-    # Prepare the training dataset.
-    train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-    train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)
+        en_tokens = tokenizers.en.tokenize(en_examples)
+        lengths.append(en_tokens.row_lengths())
+        print('.', end='', flush=True)
 
-    # Create logger
-    log_file, file_name = create_csv_logger_cb(folder_name=filename)
+    all_lengths = np.concatenate(lengths)
 
-    # print headline
-    log_string = "loss_train;acc_train;loss_val;acc_val;loss_test;acc_test;rank1;rank2;rank3;rank4\n"
-    with open(file_name, "a") as log:
-        log.write(log_string)
+    plt.hist(all_lengths, np.linspace(0, 500, 101))
+    plt.ylim(plt.ylim())
+    max_length = max(all_lengths)
+    plt.plot([max_length, max_length], plt.ylim())
+    plt.title(f'Max tokens per example: {max_length}');
+    plt.show()
 
-    # load weights
-    if load_model == 1:
-        model.load(folder_name=folder_name)
+    train_batches = make_batches(train_examples)
+    val_batches = make_batches(val_examples)
 
-    best_acc = 0
-    best_loss = 10
-    # Iterate over epochs. (Training loop)
-    for epoch in range(epochs):
-        print("Start of epoch %d" % (epoch,))
-        # Iterate over the batches of the dataset.
+    num_layers = 4
+    d_model = 128
+    dff = 512
+    num_heads = 8
+    dropout_rate = 0.1
 
-        for step, batch_train in enumerate(train_dataset):
+    learning_rate = CustomSchedule(d_model)
 
-            with tf.GradientTape() as tape:
-                out = model(batch_train[0], training=True)
-                # softmax activation for classification
-                out = tf.keras.activations.softmax(out)
-                # Compute reconstruction loss
-                loss = loss_fn(batch_train[1], out)
-                loss += sum(model.losses)  # Add KLD regularization loss
+    optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98,
+                                     epsilon=1e-9)
 
-            if step == 0:
-                # Network monotoring and verbosity
-                loss_metric.update_state(loss)
-                prediction = tf.math.argmax(out, 1)
-                acc_metric.update_state(prediction, batch_train[1])
+    temp_learning_rate_schedule = CustomSchedule(d_model)
 
-                loss_value = loss_metric.result().numpy()
-                acc_value = acc_metric.result().numpy()
-                print("----- Training Metrics  ----")
+    loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+        from_logits=True, reduction='none')
 
-                print("step %d: mean loss S-Step = %.4f" % (step, loss_value))
-                print("Accuracy: " + str(acc_value))
-                print("Loss: " + str(loss_value))
-                print("Current Rank: " + str(int(model.dlraBlockInput.low_rank)) + " | " + str(
-                    int(model.dlraBlock1.low_rank)) + " | " + str(
-                    int(model.dlraBlock2.low_rank)) + " | " + str(int(model.dlraBlock3.low_rank)) + " )")
-                # Reset metrics
-                loss_metric.reset_state()
-                acc_metric.reset_state()
+    train_loss = tf.keras.metrics.Mean(name='train_loss')
+    train_accuracy = tf.keras.metrics.Mean(name='train_accuracy')
 
-                print("----- Validation Metrics----")
-                # Compute vallidation loss and accuracy
-                loss_val = 0
-                acc_val = 0
+    # build model
+    transformer = Transformer(
+        num_layers=num_layers,
+        d_model=d_model,
+        num_heads=num_heads,
+        dff=dff,
+        input_vocab_size=tokenizers.pt.get_vocab_size().numpy(),
+        target_vocab_size=tokenizers.en.get_vocab_size().numpy(),
+        rate=dropout_rate)
 
-                # Validate model
-                out = model(x_val, step=0, training=True)
-                out = tf.keras.activations.softmax(out)
-                loss_val = loss_fn(y_val, out)
-                loss_metric.update_state(loss_val)
-                loss_val = loss_metric.result().numpy()
+    checkpoint_path = './checkpoints/train'
 
-                prediction = tf.math.argmax(out, 1)
-                acc_metric.update_state(prediction, y_val)
-                acc_val = acc_metric.result().numpy()
-                print("Accuracy: " + str(acc_val))
-                print("Loss: " + str(loss_val))
-                # save current model if it's the best
-                if acc_val >= best_acc and loss_val <= best_loss:
-                    best_acc = acc_val
-                    best_loss = loss_val
-                    print("new best model with accuracy: " + str(best_acc) + " and loss " + str(best_loss))
+    # store model weights in checkpoints
+    ckpt = tf.train.Checkpoint(transformer=transformer,
+                            optimizer=optimizer)
 
-                    model.save(folder_name=folder_name_best)
-                model.save(folder_name=folder_name)
-                # Reset metrics
-                loss_metric.reset_state()
-                acc_metric.reset_state()
+    ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
 
-                print("----- Test Metrics (not used for early stopping) ----")
+    # if a checkpoint exists, restore the latest checkpoint.
+    if ckpt_manager.latest_checkpoint:
+        ckpt.restore(ckpt_manager.latest_checkpoint)
+        print('Latest checkpoint restored!!')
 
-                # Test model
-                out = model(x_test, step=0, training=True)
-                out = tf.keras.activations.softmax(out)
-                loss_test = loss_fn(y_test, out)
-                loss_metric.update_state(loss_test)
-                loss_test = loss_metric.result().numpy()
+    EPOCHS = 20
 
-                prediction = tf.math.argmax(out, 1)
-                acc_metric.update_state(prediction, y_test)
-                acc_test = acc_metric.result().numpy()
-                print("Accuracy: " + str(acc_test))
-                print("Loss: " + str(loss_test))
-                # Reset metrics
-                loss_metric.reset_state()
-                acc_metric.reset_state()
-                print("-------------------------------------\n\n")
+    # The @tf.function trace-compiles train_step into a TF graph for faster
+    # execution. The function specializes to the precise shape of the argument
+    # tensors. To avoid re-tracing due to the variable sequence lengths or variable
+    # batch sizes (the last batch is smaller), use input_signature to specify
+    # more generic shapes.
 
-            # Gradient updates for k step
-            grads = tape.gradient(loss, model.trainable_weights)
-            optimizer.apply_gradients(zip(grads, model.trainable_weights))
+    for epoch in range(EPOCHS):
+        start = time.time()
 
-        # Log Data of current epoch
-        log_string = str(loss_value) + ";" + str(acc_value) + ";" + str(
-            loss_val) + ";" + str(acc_val) + ";" + str(
-            loss_test) + ";" + str(acc_test) + ";" + str(
-            int(model.dlraBlockInput.low_rank)) + ";" + str(
-            int(model.dlraBlock1.low_rank)) + ";" + str(int(model.dlraBlock2.low_rank)) + ";" + str(
-            int(model.dlraBlock3.low_rank)) + "\n"
-        with open(file_name, "a") as log:
-            log.write(log_string)
-        print("Epoch Data :" + log_string)
+        train_loss.reset_states()
+        train_accuracy.reset_states()
+
+        # inp -> portuguese, tar -> english
+        for (batch, (inp, tar)) in enumerate(train_batches):
+            train_step(inp, tar)
+
+            if batch % 50 == 0:
+                print(f'Epoch {epoch + 1} Batch {batch} Loss {train_loss.result():.4f} Accuracy {train_accuracy.result():.4f}')
+
+        if (epoch + 1) % 5 == 0:
+            ckpt_save_path = ckpt_manager.save()
+            print(f'Saving checkpoint for epoch {epoch+1} at {ckpt_save_path}')
+
+        print(f'Epoch {epoch + 1} Loss {train_loss.result():.4f} Accuracy {train_accuracy.result():.4f}')
+
+        print(f'Time taken for 1 epoch: {time.time() - start:.2f} secs\n')
+
 
     return 0
 
+train_step_signature = [
+    tf.TensorSpec(shape=(None, None), dtype=tf.int64),
+    tf.TensorSpec(shape=(None, None), dtype=tf.int64),
+]
 
-def normalize_img(image, label):
-    """Normalizes images: `uint8` -> `float32`."""
-    return tf.cast(image, tf.float32) / 255., label
+
+@tf.function(input_signature=train_step_signature)
+def train_step(inp, tar):
+    tar_inp = tar[:, :-1]
+    tar_real = tar[:, 1:]
+
+    with tf.GradientTape() as tape:
+        predictions, _ = transformer([inp, tar_inp],
+                                    training = True)
+        loss = loss_function(tar_real, predictions)
+
+    gradients = tape.gradient(loss, transformer.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, transformer.trainable_variables))
+
+    train_loss(loss)
+    train_accuracy(accuracy_function(tar_real, predictions))
 
 
 if __name__ == '__main__':
