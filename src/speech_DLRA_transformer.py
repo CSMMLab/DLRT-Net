685d1ea9adee3408175c9a767b2aaae2dc1badb8
Steffen Schotthöfer <steffen.schotthoefer@kit.edu>
2022-09-12 09:30:05 +0200

    fixed bug with dlra and translator

--- src/speech_DLRA_transformer.py
+++ src/speech_DLRA_transformer.py
@@ -217,7 +217,7 @@ def train(tolerance):
 
         print(f'Time taken for 1 epoch: {time.time() - start:.2f} secs\n')
 
-    test_transformer(transformer, tokenizers, test_examples, filename)
+    test_transformer(transformer, tokenizers, test_examples, filename, dlra=True)
     return 0
 
 
